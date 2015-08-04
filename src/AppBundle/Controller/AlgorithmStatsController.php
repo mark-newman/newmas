ab3261898572c46e8aada51d57b5ed7064945240
Mark Newman <newman.mark84@gmail.com>
2015-08-04 09:48:37 +0100

    added fixtures list into algorithm results

--- src/AppBundle/Controller/AlgorithmStatsController.php
+++ src/AppBundle/Controller/AlgorithmStatsController.php
@@ -45,12 +45,12 @@ class AlgorithmStatsController extends Controller {
         $league = $request->get('league_code');
         $season = $request->get('season');
         $algorithm = $request->get('algorithm');
-        $algorithm_success_score = $request->get('algorithm_success_value');
+        $algorithm_success_value = $request->get('algorithm_success_value');
         $minimum_history = $request->get('minimum_history');
         $include_x_league_form = $request->get('include_x_league_form');
         $minimum_league_form = $request->get('minimum_league_form');
 
-        if(!$league || !$season || !$algorithm || $algorithm_success_score === '' || $minimum_history === '' || $include_x_league_form === '' || $minimum_league_form === ''){
+        if(!$league || !$season || !$algorithm || $algorithm_success_value === '' || $minimum_history === '' || $include_x_league_form === '' || $minimum_league_form === ''){
             die;
         }
 
@@ -61,7 +61,7 @@ class AlgorithmStatsController extends Controller {
         $correct = 0;
         $incorrect = 0;
         $prediction_count = 0;
-        $count = 0;
+        $fixture_data = array();
 
         foreach ($matches as $match)
         {
@@ -71,19 +71,22 @@ class AlgorithmStatsController extends Controller {
             $historical_fixtures = $this->em->getRepository('AppBundle:Result')->findHistoricalFixturesHomeTeamAndAwayTeam($home_team_id, $away_team_id, $matchday);
 
             $algorithm_data = $this->get('algorithm_service')->goalsGaloreAlgorithmFunction($home_team_id, $away_team_id, $historical_fixtures, $matchday, $minimum_history, $include_x_league_form, $minimum_league_form);
-
-            if(array_key_exists('error', $algorithm_data) || $algorithm_data['score'] < $algorithm_success_score){
+            if(array_key_exists('error', $algorithm_data) || ($algorithm_data['score'] < $algorithm_success_value)){
+                continue;
             }elseif($match->getHomeScore()>0 && $match->getAwayScore()>0){
                 $prediction_count++;
                 $correct++;
+                $fixture_data[$match->getId()]['fixture'] = $match;
+                $fixture_data[$match->getId()]['history'] = $historical_fixtures;
             }else{
                 $prediction_count++;
                 $incorrect++;
+                $fixture_data[$match->getId()]['fixture'] = $match;
+                $fixture_data[$match->getId()]['history'] = $historical_fixtures;
             }
-            $count++;
         }
 
-        return $this->render('algorithm-stats/partials/algorithmResults.html.twig', compact('league', 'season', 'algorithm_success_score', 'matches','prediction_count', 'correct'));
+        return $this->render('algorithm-stats/partials/algorithmResults.html.twig', compact('league', 'season', 'algorithm_success_score', 'matches','prediction_count', 'correct', 'fixture_data', 'algorithm_success_value'));
 
     }
 
